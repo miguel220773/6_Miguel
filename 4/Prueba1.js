ea3a112b9226a46f68424e51eb8edc44fca5e2cb
miguel220773 <miguelvs2207@gmail.com>
2025-12-05 13:52:01 +0100

    java script

---
+++ 4/Prueba1.js
@@ -0,0 +1,12 @@
+//soy un comentario en JavaScript
+// alert('prueba de código');
+var mensaje = 'hola mundo quetal';
+console.log(mensaje);
+console.log(document);
+console.log(localStorage);
+var esViernes = true;
+console.log('esViernes y queremos irnos: ' + esViernes);
+debugger
+if (esViernes == true) {
+console.log('es viernes, nos vamos de fiesta');
+}
