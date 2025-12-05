afb7673f19a798c2f5b1bd7b7a3e1c32ce2e4c21
miguel220773 <miguelvs2207@gmail.com>
2025-12-05 14:14:43 +0100

    hoy

--- 4/Prueba1.js
+++ 4/Prueba1.js
@@ -2,11 +2,19 @@
 // alert('prueba de código');
 var mensaje = 'hola mundo quetal';
 console.log(mensaje);
+console.log(mensaje);
 console.log(document);
 console.log(localStorage);
+
+
 var esViernes = true;
-console.log('esViernes y queremos irnos: ' + esViernes);
-debugger
+console.log('Es Viernes y queremos irnos: ' + esViernes);
+//debugger
 if (esViernes == true) {
 console.log('es viernes, nos vamos de fiesta');
 }
+
+var nombreUsuario = prompt('introduzca su nombre de usuario: ');
+console.log('el nombre de usuario es: ', nombreUsuario);
+document.write('<h1>' + nombreUsuario + '</h1>');
+ 
